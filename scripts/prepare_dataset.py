11eb5c0bd22328429428d6f1507f6221338240ee
mzgubic <miha.zgubic@gmail.com>
2020-03-10 18:17:33 +0000

    merge shops and prods data

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -21,33 +21,9 @@ def aggregate(year):
     @utils.timeit
     def load_df():
         print('Loading dataframes')
-        shops = prep.load_shops()
-        prods = prep.load_products()
-        sales = prep.load_sales(infile, nrows=1000)
-        return shops, prods, sales
-    shops, prods, sales = load_df()
-
-    ## merge the shops and product information
-    #@utils.timeit
-    #def merge(sales, df, on, gb, features):
-    #    if isinstance(on, str):
-    #        on = [on]
-    #    if isinstance(gb, str):
-    #        gb = [gb]
-    #    keeps = list(set(features).union(set(on + gb))) # avoid duplicates
-    #    return sales.merge(df[keeps], how='left', on=on)
-
-    #print('Merging shop information')
-    #shop_features = ['Franchise', 'NUniqueProductsSold', 'NTotalProductsSold']
-    #shop_merge_on = 'StoreKey'
-    #shop_group_by = ['StoreKey']
-    ##sales = merge(sales, shops, shop_merge_on, shop_group_by, shop_features)
-
-    #print('Merging product information')
-    #prod_features = ['Gender', 'Season', 'OriginalListedPrice']
-    #prod_merge_on = 'EAN' # exact identifier of the item, including colour and size
-    #prod_group_by = ['EAN' if args.EAN else 'ProductID'] # aggregate over colour and size
-    ##sales = merge(sales, prods, prod_merge_on, prod_group_by, prod_features)
+        sales = prep.load_sales(infile, nrows=1000) #TODO
+        return sales
+    sales = load_df()
 
     # compute the week of the year
     @utils.timeit
@@ -63,10 +39,9 @@ def aggregate(year):
         print('Grouping and aggregating')
         targets = ['Volume']
         features = ['EAN', 'Week', 'StoreKey']
-
         df = sales.groupby(features)[targets].sum()
-
         return df
+
     df = aggregate(sales)
 
     # hack, sorry
@@ -123,6 +98,13 @@ def load_sales_dict(year):
 
 
 @utils.timeit
+def merge(skeleton, df, features, on):
+    print('Merging features')
+    keeps = set(features).union(set([on]))
+    return skeleton.merge(df[keeps], how='left', on=on)
+
+
+@utils.timeit
 def sample(year, sample):
 
     print('Sampling sales dataset')
@@ -138,8 +120,17 @@ def sample(year, sample):
     sales = load_sales_dict(year)
     skeleton = fill_skeleton(skeleton, sales)
 
-    # merge the product and store information
-    # TODO
+    # merge shops features
+    shops = prep.load_shops()
+    shop_features = ['Franchise', 'NUniqueProductsSold', 'NTotalProductsSold']
+    shop_on = 'StoreKey'
+    skeleton = merge(skeleton, shops, shop_features, shop_on)
+
+    # merge the product features
+    prods = prep.load_products()
+    prod_features = ['Gender', 'Season', 'OriginalListedPrice']
+    prod_on = 'EAN'
+    skeleton = merge(skeleton, prods, prod_features, prod_on)
 
     return skeleton
 
